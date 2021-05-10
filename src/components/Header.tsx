189c1d5586fbdbdfd2d07393760316ade3317744
bepyan <rlantia97@kumoh.ac.kr>
2021-05-10 22:15:29 +0900

    🦥 init

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+interface Props {
+
+}
+
+const Header = (props: Props) => {
+    return (
+        <p style={{ textAlign: "center", fontSize: 50, fontWeight: "bold" }}>
+            💻 Happy Hacking
+        </p>
+    )
+}
+
+export default Header
